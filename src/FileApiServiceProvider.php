73ea1531f984b4b5007c4b9247939b58522da2da
gbrock <contact@gbrock.com>
2015-07-01 10:20:42 -0400

    Booting/publishing new features from service provider

--- src/FileApiServiceProvider.php
+++ src/FileApiServiceProvider.php
@@ -2,15 +2,33 @@
 
 namespace GridPrinciples\FileApi;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class FileApiServiceProvider extends ServiceProvider {
 
     public function boot()
     {
+        // publish the config file
         $this->publishes([
             __DIR__.'/../resources/config.php' => config_path('files.php'),
         ]);
+
+        // publish the migration
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('/migrations')
+        ], 'migrations');
+
+        // bind the File model to a route keyword
+        Route::bind('file', function($value)
+        {
+            if(!$found = File::where('file_hash', $value)->first())
+            {
+                return abort(404);
+            }
+
+            return $found;
+        });
     }
 
     /**
@@ -20,10 +38,12 @@ class FileApiServiceProvider extends ServiceProvider {
      */
     public function register()
     {
+        // merge the config file with the user's
         $this->mergeConfigFrom(
-            __DIR__.'/../resources/config.php', config_path('files.php')
+            __DIR__.'/../config/files.php', config_path('files.php')
         );
 
+        // bind the API to the container
         $this->app->bind('file_api', function()
         {
             return new \GridPrinciples\FileApi\Api;
