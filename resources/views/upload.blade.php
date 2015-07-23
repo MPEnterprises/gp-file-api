eff49a6516f7ef0fa40d3d5729a0a6d1247af4f9
gbrock <contact@gbrock.com>
2015-07-23 15:58:07 -0400

    Pass variables through the surface Blade file to the deeper one

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,6 +1,13 @@
 @if(View::exists('form::html'))
     @include('form::html', [
-        'content' => view('file::single_upload'),
+        'content' => view('file::single_upload', [
+            // Explicitly pass in the variables which might be used by the rendered view
+            'name' => isset($name) ? $name : null,
+            'max' => isset($max) ? $max : null,
+            'max_width' => isset($max_width) ? $max_width : null,
+            'max_height' => isset($max_height) ? $max_height : null,
+            'files' => isset($files) ? $files : null,
+        ]),
     ])
 @else
     @include('file::single_upload')
